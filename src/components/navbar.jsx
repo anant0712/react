1e6494e80bd25a2f6bed2c0fc13dc1e14c7a6863
anant0712 <anantmishra0761@gmail.com>
2026-07-16 09:50:56 +0530

    Components done

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const navbar = () => {
+  return (
+    <div>
+        <div className='navbar'>
+            <h1>My Navbar</h1>
+        </div>
+    </div>
+  )
+}
+
+export default navbar
